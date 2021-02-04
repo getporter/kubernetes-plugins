ecadb1d9c1b9774e01b6c5a0484cf9a48dcbcefd
Simon Davies <simongdavies@users.noreply.github.com>
2021-02-04 16:25:37 +0000

    updated test script
    Signed-off-by: Simon Davies <simongdavies@users.noreply.github.com>

--- build/run-integration-tests.sh
+++ build/run-integration-tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-trap 'make -f Makefile.kind delete-kind-cluster' EXIT
-make -f Makefile.kind install-kind create-kind-cluster
+trap 'make -f ./tests/Makefile.kind delete-kind-cluster' EXIT
+make -f ./tests/Makefile.kind install-kind create-kind-cluster
 make test-integration
 make test-in-kubernetes
